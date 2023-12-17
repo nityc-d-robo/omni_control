2108ceda7f58d7e2f60d062955b41ad64568af79
ezomo <kiruruman@gmail.com>
2023-12-17 12:36:01 +0900

    Supports only complete parallel transfers in a manner of speaking built on an empty theory on the table

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ enum Chassis {
 }
 
 
-const OMNI_DIA:f64 =  0.1;
+// const OMNI_DIA:f64 =  0.1;
+const  MAX_PAWER:f64 = 160.;
 
 fn main() -> Result<(), DynError>{
     let ctx = Context::new()?;
@@ -22,7 +23,7 @@ fn main() -> Result<(), DynError>{
     let subscriber = node.create_subscriber::<msg::Twist>("cmd_vel", None)?;
     let publisher = node.create_publisher::<drobo_interfaces::msg::MdLibMsg>("md_driver_topic", None)?;
 
-    let _logger = Logger::new("omni_controll");
+    let logger = Logger::new("omni_controll");
 
     let mut selector = ctx.create_selector()?;
 
@@ -33,6 +34,7 @@ fn main() -> Result<(), DynError>{
             Box::new(move |msg| {
                 // pr_info!(logger, "receive: {:?}", msg.linear);
                 let topic_callback_data = topic_callback(msg);
+                // safe_drive::pr_info!(logger,"an:{},pa:{}",topic_callback_data[0],topic_callback_data[1]);
                 move_chassis(topic_callback_data[0],topic_callback_data[1],topic_callback_data[2],&publisher);
             })
         },
@@ -44,62 +46,49 @@ fn main() -> Result<(), DynError>{
 }
 
 
-fn topic_callback(msg: subscriber::TakenMsg<Twist>) -> [f64;3] {
+fn topic_callback(msg: subscriber::TakenMsg<Twist>) -> [f64;3]{
 
-    let xrpm:f64 = msg.linear.x / (2.0 * PI * OMNI_DIA) * 60.0;
-    let yrpm:f64 = msg.linear.y / (2.0 * PI * OMNI_DIA) * 60.0;
-    let yaw:f64 = msg.angular.z;
+    let theta:f64 = (
+            if -msg.linear.x >0. && msg.linear.y >0. {
 
-    return [xrpm,yrpm,yaw];
+                0.
+            }
+            else if -msg.linear.x < 0. {
+                PI
+            }
+            else {
+                2.*PI
+            }+(msg.linear.y / -msg.linear.x).atan()
+
+        ) * if -msg.linear.x == 0. { 0. } else { 1. };
+    
+    let pawer = (msg.linear.x.powf(2.) + msg.linear.y.powf(2.)).sqrt();
+    
+
+    [theta,pawer,msg.angular.z]
+    
 }
 
-fn move_chassis(_x:f64, _y:f64, _yaw:f64,publisher:&Publisher<MdLibMsg>){
+fn move_chassis(_theta:f64, _pawer:f64, _yaw:f64,publisher:&Publisher<MdLibMsg>){
 
     let mut motor_power:[i32;4] = [0;4];
 
-    let power_level: f64 =(_x * _x + _y * _y).sqrt();
+    motor_power[Chassis::FR as usize] = (_theta-(PI/4.)).sin() as i32; 
+    motor_power[Chassis::BR as usize] = (_theta-(PI* 3./4.)).sin() as i32;
+    motor_power[Chassis::BL as usize] = (_theta-(PI * 5./4.)).sin() as i32;
+    motor_power[Chassis::FL as usize] = (_theta-(PI * 7./4.)).sin() as i32;
 
-    if power_level == 0.0 {
 
-        for i in 0..4{
-            send_pwm(i,0,false,0,publisher);
-        }
-    }
-
-    let yaw = 
-        if (_yaw * 100.0) > 50.0{
-            0.5
-        }
-        else if (_yaw * 100.0) < -50.0{
-            -0.5
-        }
-        else{
-            _yaw
-        };
-
-    motor_power[Chassis::BL as usize] = (-(PI / 4.0).sin() * _x + (PI / 4.0).cos() * _y + yaw * 10.0) as i32;
-    motor_power[Chassis::BR as usize] = (-(3.0 * PI / 4.0).sin() * _x + (3.0 * PI / 4.0).cos() * _y + yaw * 10.0) as i32;
-    motor_power[Chassis::FR as usize] = (-(5.0 * PI / 4.0).sin() * _x + (5.0 * PI / 4.0).cos() * _y + yaw * 10.0) as i32;
-    motor_power[Chassis::FL as usize] = (-(7.0 * PI / 4.0).sin() * _x + (7.0 * PI / 4.0).cos() * _y + yaw * 10.0) as i32;
-
+    let max_pawer = MAX_PAWER*(PI/4.).sin();
     for i in 0..4 {
-        let mut _power :i32 ;
-        let mut _phase:bool;
+        motor_power[i] *= (_pawer/max_pawer) as i32;
 
-        if motor_power[i] > 0 {
-          _phase = false;
-          _power = motor_power[i];
-        } else {
-          _phase = true;
-          _power = -motor_power[i];
-        }
-
-        send_pwm(i as u32,0,_phase, _power,publisher);
+        send_pwm(i as u32,0,motor_power[i]>0, motor_power[i] as u32,publisher);
     }
     
 }
 
-fn send_pwm(_address:u32, _semi_id:u32,_phase:bool,_power:i32,publisher:&Publisher<MdLibMsg>){
+fn send_pwm(_address:u32, _semi_id:u32,_phase:bool,_power:u32,publisher:&Publisher<MdLibMsg>){
     let mut msg = drobo_interfaces::msg::MdLibMsg::new().unwrap();
     msg.address = _address as u8;
     msg.semi_id = _semi_id as u8;
