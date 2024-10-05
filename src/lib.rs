a34c0778fbdbd46b50bf71824fcc749e64af1cf2
ezomo <ezomo1daniel@gmail.com>
2024-10-05 13:35:37 +0900

    制御式完成？

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,13 @@ pub struct Tire {
 pub struct Chassis {
     pub fl: Tire,
     pub fr: Tire,
-    pub br: Tire,
-    pub bl: Tire,
+    pub b: Tire,
 }
 
 pub struct OmniSetting {
     pub chassis: Chassis,
-    pub max_pawer_input: f64,
-    pub max_pawer_output: f64,
+    pub max_power_input: f64,
+    pub max_power_output: f64,
     pub max_revolution: f64,
 }
 
@@ -24,30 +23,31 @@ impl OmniSetting {
         let theta: f64 = linear_y.atan2(-linear_x);
         let power: f64 = (linear_x.powf(2.) + linear_y.powf(2.))
             .sqrt()
-            .min(self.max_pawer_input);
+            .min(self.max_power_input);
         [theta, power]
     }
 
-    pub fn move_chassis(&self, linear_x: f64, linear_y: f64, angular_z: f64) -> HashMap<usize, f64> {
+    pub fn move_chassis(
+        &self,
+        linear_x: f64,
+        linear_y: f64,
+        angular_z: f64,
+    ) -> HashMap<usize, f64> {
         let _revolution = angular_z;
-        let [_theta, _pawer] = &self.first_step(linear_x, linear_y);
+        let [_theta, _power] = &self.first_step(linear_x, linear_y);
 
         let mut motor_power: HashMap<usize, f64> = HashMap::new();
         motor_power.insert(
             self.chassis.fr.id,
-            (_theta - (PI * 1. / 4.)).sin() * self.chassis.fr.raito,
+            (_theta - (PI * 1. / 6.)).sin() * self.chassis.fr.raito,
         );
         motor_power.insert(
             self.chassis.fl.id,
-            (_theta + (PI * 5. / 4.)).sin() * self.chassis.fl.raito,
+            (_theta - (PI * 5. / 6.)).sin() * self.chassis.fl.raito,
         );
         motor_power.insert(
-            self.chassis.br.id,
-            (_theta + (PI * 1. / 4.)).sin() * self.chassis.br.raito,
-        );
-        motor_power.insert(
-            self.chassis.bl.id,
-            (_theta + (PI * 3. / 4.)).sin() * self.chassis.bl.raito,
+            self.chassis.b.id,
+            (_theta - (PI * 9. / 6.)).sin() * self.chassis.b.raito,
         );
 
         let standard_power: f64 = {
@@ -62,16 +62,48 @@ impl OmniSetting {
         let mut re_motor_power = HashMap::new();
         for i in motor_power.keys() {
             let mut _tmp = 0.;
-            _tmp = self.max_pawer_output * (_pawer / self.max_pawer_input) * motor_power[i]
+            _tmp = self.max_power_output * (_power / self.max_power_input) * motor_power[i]
                 / standard_power
-                + self.max_pawer_output * (_revolution / self.max_revolution);
+                + self.max_power_output * (_revolution / self.max_revolution);
 
-            _tmp = _tmp.max(-self.max_pawer_output);
-            _tmp = _tmp.min(self.max_pawer_output);
+            _tmp = _tmp.max(-self.max_power_output);
+            _tmp = _tmp.min(self.max_power_output);
 
             re_motor_power.insert(*i, _tmp);
         }
 
         re_motor_power
+    }
+}
+
+#[test]
+fn test() {
+    const CHASSIS: Chassis = Chassis {
+        fl: Tire { id: 0, raito: 1. },
+        fr: Tire { id: 1, raito: 1. },
+        b: Tire { id: 2, raito: 1. },
+    };
+
+    const MAX_POWER_INPUT: f64 = 1.;
+    const MAX_POWER_OUTPUT: f64 = 1.;
+    const MAX_REVOLUTION: f64 = 5400.;
+
+    let omni_setting = OmniSetting {
+        chassis: CHASSIS,
+        max_power_input: MAX_POWER_INPUT,
+        max_power_output: MAX_POWER_OUTPUT,
+        max_revolution: MAX_REVOLUTION,
+    };
+
+    let motor_power = omni_setting.move_chassis(-1., 0., 0.);
+
+    let y = motor_power[&1] * (PI * 1. / 6.).sin() - motor_power[&0] * (PI * 5. / 6.).sin();
+    let x = motor_power[&1] * (PI * 1. / 6.).cos() - motor_power[&0] * (PI * 5. / 6.).cos()
+        + motor_power[&2] * (PI * 9. / 6.).cos();
+
+    println!("{:?}", (x, y));
+
+    for i in motor_power {
+        println!("{:?}", i)
     }
 }
